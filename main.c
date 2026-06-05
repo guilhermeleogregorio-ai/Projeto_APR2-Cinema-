63f810c1a162bda502c518d50621c44669dd0e49
Guilherme Gregorio <guilhermeleogregorio@gmail.com>
2026-06-05 18:45:46 -0300

    fix: Mudança no sistena de busca_sessoes e relatorio Sessoes, usando inteiro ao inves de strigns.

--- main.c
+++ main.c
@@ -26,11 +26,38 @@ typedef struct {
 typedef struct {
     int codFilme;    // Chave composta (Filme + Sala + Data + Hora)
     int codSala;
-    char data[11];  // "DD/MM/AAAA"
-    char horario[6]; // "HH:MM"
+    int data;  
+    int horario; 
     float preco;
 } Sessao;
 
+// Funções auxiliares
+int stringDataParaInt(char *dataStr) {
+    int dia, mes, ano;
+    sscanf(dataStr, "%d/%d/%d", &dia, &mes, &ano);
+    return ano * 10000 + mes * 100 + dia;
+}
+// Converte int para "DD/MM/AAAA" (para exibir)
+void intParaStringData(int dataInt, char *saida) {
+    int ano = dataInt / 10000;
+    int mes = (dataInt % 10000) / 100;
+    int dia = dataInt % 100;
+    sprintf(saida, "%02d/%02d/%04d", dia, mes, ano);
+}
+
+// Converte "HH:MM" para int (ex: 20:00 -> 2000)
+int stringHorarioParaInt(char *horarioStr) {
+    int hora, minuto;
+    sscanf(horarioStr, "%d:%d", &hora, &minuto);
+    return hora * 100 + minuto;
+}
+
+// Converte int para "HH:MM" (para exibir)
+void intParaStringHorario(int horarioInt, char *saida) {
+    int hora = horarioInt / 100;
+    int minuto = horarioInt % 100;
+    sprintf(saida, "%02d:%02d", hora, minuto);
+}
 
 // Variávies globais
 Sala *salas= NULL;
@@ -60,7 +87,7 @@ void alterar_salas();
 void excluir_salas();
 
 void listar_filmes();
-int buscar_filmes(int codigo);
+int buscar_filmes(int codigo) ;
 void incluir_filmes();
 void alterar_filmes();
 void excluir_filmes();
